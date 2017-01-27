9e4f35d9dd854875ffd7633146be528ed565745e
mblair415 <mblair415@gmail.com>
2017-01-27 15:31:33 -0800

    managing sessions for existing users - about to start

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'users#index'
+
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+end
